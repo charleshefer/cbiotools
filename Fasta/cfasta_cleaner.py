7c144d215e20584daa709ee1673ed5345fcc0132
Charles Hefer <heferc@invbfodp01.agresearch.co.nz>
2018-11-28 09:27:29 +1300

    cfasta_cleaner.py v0.1 complete

--- Fasta/cfasta_cleaner.py
+++ Fasta/cfasta_cleaner.py
@@ -1,11 +1,14 @@
 ###############################################################################
-#Jessica Gathercole
-#Annotation of peptides
+#cfasta_cleaner
+#Reads in a fasta formatted file, writes out a standard format using
+#biopython
 #
-#@author:charles.hefer@agresearch.co.nz
+#@requires biopython
+#@author:charles.hefer@gmail.com
 #@version:0.1
 ###############################################################################
 import optparse
+from Bio import SeqIO
 
 
 def __main__():
@@ -21,6 +24,18 @@ def __main__():
 		parser.error("Need to specify the input file")
 	if not options.output:
 		parser.error("Need to specify the output file")
+	
+	#Read into RAM, will bomb if there are Gbs of entries
+	#but this has not happened yet.
+	#Better than IO overhead for most cases
+	fasta_records = []
+	
+	with open(options.input, "r") as handle:
+		for record in SeqIO.parse(handle, "fasta"):
+			fasta_records.append(record)
+	
+	with open(options.output, "w") as outhandle:
+		SeqIO.write(fasta_records, outhandle, "fasta")
 		
 if __name__ == "__main__":
 	__main__()
