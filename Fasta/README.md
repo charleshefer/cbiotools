cd6bf8505c345130e4a57090defb86b2ac8222ba
Charles Hefer <heferc@invbfodp01.agresearch.co.nz>
2018-11-28 09:16:04 +1300

    Added the backbone python script, and the README

---
+++ Fasta/README.md
@@ -0,0 +1,10 @@
+# Fasta Tools
+
+Some tools and scripts that assists with manupilating FASTA formatted files.
+
+## Description
+
+### cfasta_cleaner.py
+Takes a fasta file, and rewrites it in a standard format. This ensures that there are no white spaces in the file and that the sequence entries are of the same lenght. This came about when an error popped up using cuffmerge (see http://seqanswers.com/forums/archive/index.php/t-14419.html for a similar issue) with a 'dirty' fasta file.
+
+
---
+++ Fasta/cfasta_cleaner.py
@@ -0,0 +1,27 @@
+###############################################################################
+#Jessica Gathercole
+#Annotation of peptides
+#
+#@author:charles.hefer@agresearch.co.nz
+#@version:0.1
+###############################################################################
+import optparse
+
+
+def __main__():
+	"""Parse the cmd lne options"""
+	parser = optparse.OptionParser()
+	parser.add_option("-i", "--input", default=None, dest="input",
+					  help="The input file")
+	parser.add_option("-o", "--ouput", default=None, dest="output",
+					  help="The output file")
+	(options, args) = parser.parse_args()
+	
+	if not options.input:
+		parser.error("Need to specify the input file")
+	if not options.output:
+		parser.error("Need to specify the output file")
+		
+if __name__ == "__main__":
+	__main__()
+	
