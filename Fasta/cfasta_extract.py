7c09c1f9eb41ff3a40384ba3b52c9669bdb8fa02
Charles Hefer <charles.hefer@gmail.com>
2019-02-01 14:08:17 +1300

    added ability to provide a list

--- Fasta/cfasta_extract.py
+++ Fasta/cfasta_extract.py
@@ -7,6 +7,7 @@
 ###############################################################################
 import optparse
 from Bio import SeqIO
+import sys
 
 def __main__():
 	"""Parse the cmd lne options"""
@@ -17,6 +18,8 @@ def __main__():
 					  help="The output file")
 	parser.add_option("-m", "--match", default=None, dest="match",
 					  help="The partial match for the fasta entry, case sensitive")
+	parser.add_option("-f", "--file", default=None, dest="file",
+					help = "A filename with a list of entries to match")
 	(options, args) = parser.parse_args()
 	
 	
@@ -24,14 +27,26 @@ def __main__():
 		parser.error("Need to specify the input file")
 	if not options.output:
 		parser.error("Need to specify the output file")
-	if not options.match:
-		parser.error("Need to specify some part of an entry name to match")
-		
+	if not options.match and not options.file:
+		parser.error("Need to specify either some part of an entry name (-m) or a filename of entries (-f) to match")
+
+	if options.file:
+		with open(options.file, "r") as handle:
+			entries = [e.rstrip() for e in handle]
+
+
+
 	fasta_records = []
 	with open(options.input, "r") as handle:
 		for record in SeqIO.parse(handle, "fasta"):
-			if options.match in record.name:
-				fasta_records.append(record)
+			if options.match:
+				if options.match in record.name:
+					fasta_records.append(record)
+			if options.file:
+				for entry in entries:
+					if entry in record.name:
+						fasta_records.append(record)
+						break
 				
 	with open(options.output, "w") as outhandle:
 		SeqIO.write(fasta_records, outhandle, "fasta")
